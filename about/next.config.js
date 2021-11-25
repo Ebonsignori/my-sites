35f13e8f6fbd69fea32cbaf8e8976019cff3c41c
Evan Bonsignori <evanabonsignori@gmail.com>
2021-11-24 17:55:21 -0800

    add image support with pages hosting

--- about/next.config.js
+++ about/next.config.js
@@ -1,3 +1,11 @@
-module.exports = {
-  reactStrictMode: true,
+const options = {
+  reactStrictMode: true, 
+};
+
+if (process.env.NODE_ENV !== "development") {
+  options.loader = ({ src, width, quality }) => {
+    return `https://ebonsignori.github.io/my-about/images/${src}`
+  }
 }
+
+module.exports = options;
