82dae40ac59c617f067555c7f51b261e36f13efd
Evan Bonsignori <evanabonsignori@gmail.com>
2021-12-05 14:37:42 -0800

    add Google analytics

--- music/pages/_app.js
+++ music/pages/_app.js
@@ -1,8 +1,11 @@
 import Head from "next/head";
+import Script from "next/script";
 
 import { BREAKPOINTS } from "../../shared/utils/breakpoints";
 import { getImageSource } from "../../shared/utils/image";
 import GlobalStyle from "../src/utils/global-styles";
+
+const TRACKING_ID = "G-SK34YBMR49";
 
 function MyApp({ Component, pageProps }) {
   // TODO: Use different image for writing site
@@ -11,6 +14,31 @@ function MyApp({ Component, pageProps }) {
     BREAKPOINTS[2],
     true
   );
+  let analytics = null;
+  if (process.env.NODE_ENV === "production") {
+    analytics = (
+      <>
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=${TRACKING_ID}`}
+        />
+        <Script
+          id="gtag-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+        />
+      </>
+    );
+  }
   return (
     <>
       <GlobalStyle />
@@ -24,7 +52,9 @@ function MyApp({ Component, pageProps }) {
         <meta property="og:image" content={imagePreviewUrl} />
         <meta property="og:image:secure_url" content={imagePreviewUrl} />
         <meta property="og:image:alt" content="Night sky with stars" />
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
       </Head>
+      {analytics}
       <Component {...pageProps} />
     </>
   );
